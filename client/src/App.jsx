cd784e8d025a9f8f7c853ced14d2c7d87ad34871
sajidmehmoodtariq30 <sajidmehmoodtariq5@gmail.com>
2025-05-30 00:01:23 +0500

    React + Tailwind + ShadcnUI

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from "@/components/ui/button"
+
+
+const App = () => {
+  return (
+    <Button variant="outline">Lets GO Boy</Button>
+
+  )
+}
+
+export default App
