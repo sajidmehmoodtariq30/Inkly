3ece0e3c7db90bbd32ff35c2b3ae7d060d36ac9b
Muhammad Haseeb Iqbal <muhammadhaseebiqbal.official@gmail.ccom>
2025-06-15 13:12:56 +0500

    Remove specific CORS configuration for simplified access control

--- server/server.js
+++ server/server.js
@@ -12,17 +12,7 @@ const app = express();
 
 app.use(cookieParser());
 app.use(express.json());
-app.use(cors({
-    origin: [
-        'http://localhost:5173',
-        'https://inklyserver.vercel.app',
-        'http://localhost:3000',
-        'https://inkly-mu.vercel.app'
-    ],
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Accept-Language', 'X-CSRF-Token']
-}));
+app.use(cors());
 
 connectDB();
 
